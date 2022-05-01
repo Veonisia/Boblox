8e3dbb9c2160de0677b9046bbdad0f9349fe8f47
Veonisia <102913473+Veonisia@users.noreply.github.com>
2022-05-01 15:00:14 +0200

    Create ??.lua

---
+++ ??.lua
@@ -0,0 +1,193 @@
+local place = game.PlaceId
+
+if place == 8097148487 then
+wait(10)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/pattingbabies/blora/main/mchampions'),true))()
+end
+-- AAAAAAAAAA
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":x: Server has over 3 minutes. :mag_right:** Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+if RemainingTime < 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":white_check_mark: **PING:** ||<@&967517811645751358>|| | :sparkles: We've found a server! (-3 minutes) Account to join: `QueenOdinW`")
+wait()
+end
+
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
+wait(20)
+repeat wait() until game.ContentProvider.RequestQueueSize == 0
+local guiMinutes = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Time.Text 
+local Mins, Seconds = unpack((tostring(guiMinutes)):split(":"))
+local RemainingTime = tonumber(Mins) * 60 + tonumber(Seconds)
+
+if RemainingTime > 180 then
+function _G.SendWebHook(url, message)newBody = {["content"] = tostring(message)}; SynWebHook = {Url = url,Body = game.HttpService:JSONEncode(newBody),Method = "POST",Headers = {["content-type"] = "application/json"}};syn.request(SynWebHook)end;
+
+_G.SendWebHook("https://discord.com/api/webhooks/970065389906194514/W6yOtXO5Kj4m2pVHGWUYLADwLDp8kkj1yOLYSHY0DNyEs_rwhGh0NhCqMv9yenL32zt1", ":man_mage: Wizards have defeated Ice Golem! // Nobody joined<:fakhero:970284468688535593> | :mag_right:**Looking for another one..**")
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "Zero", false, game)
+       wait(.0)
+peanut = peanut + 1
+end --Serverhop function here
+end
