d18aa8bb11f338c73070801c0e714a727d45c89e
Veonisia <102913473+Veonisia@users.noreply.github.com>
2022-05-04 21:38:31 +0200

    Update Bosss.lua

--- Bosss.lua
+++ Bosss.lua
@@ -1,43 +1,42 @@
-
-
-
-
-
--- Boss
-_G.KeepChecking = true
-
-local bossstatus = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Status.Text
-while _G.KeepChecking do
-function boss()
-    if bossstatus == "STATUS: PREPARING" then
+-- Seakui
+local vim = game:service'VirtualInputManager'
+while wait(.0) do
+       vim:SendKeyEvent(true, "P", false, game)
+       wait(.0)
+end
+local Imput = game:GetService("UserInputService")
+    Imput.InputBegan:connect(function(inst)
+    if inst.KeyCode == Enum.KeyCode.P then -- Pressing P == keep checking
+    if bstatus == "STATUS: PREPARING" then
         local args = {[1] = "Boss",[2] = "Ice Golem"}
         game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
-    wait(15)
-local vim = game:service'VirtualInputManager'
-local peanut = 0
-while wait(0) do
-       vim:SendKeyEvent(true, "TELEPORT KEY HERE", false, game)
-       wait(.0)
-peanut = peanut + 1
-if peanut == 1
-then
-    break
+        wait(13)
+        sendAllAttacks()
+    end
 end
-end
-    attack()
-end
-task.wait()
-end
+end)
+--
+
+local bstatus = game:GetService("Workspace").Bosses["Ice Golem"].UI.SurfaceGui.Status.Text
+-- Seakui
 
 
-
--- Attack Function
-function attack()
-local args = {[1] = "UseSpell",[2] = "SPELL HERE",} -- Golden Hearts, Tornado..
-game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
-wait() -- SPELL'S COOLDOWN!
-end
-
-task.wait(15)
-attack()
+function sendAllAttacks()
+    local args = {[1] = "UseSpell",[2] = "Vengeful Heart",}
+    game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
+    wait(2)
+    local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+    local location = CFrame.new(3457, -71, -4084)
+    local humanoid = game.Players.LocalPlayer.Character.Humanoid
+    humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+    pl.CFrame = location
+    wait(.2)
+    local args = {[1] = "UseSpell",[2] = "Whirling Tornado",[3] = Vector3.new(3476, -71, -4082)}
+    game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
+    wait(2)
+    local args = {[1] = "UseSpell",[2] = "Golden Hearts",}
+    game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
+    wait(1.5)
+    local args = {[1] = "UseSpell",[2] = "Fire Wall",}
+    game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
 end
