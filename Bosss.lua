bc0985d5973840f26a21bd74a73599f583241911
Veonisia <102913473+Veonisia@users.noreply.github.com>
2022-05-04 11:15:44 +0200

    Create Bosss.lua

---
+++ Bosss.lua
@@ -0,0 +1,31 @@
+-- Boss
+local bossstatus = workspace.Bosses:FindFirstChild("Ice Golem").UI.SurfaceGui.Status.Text
+function boss()
+    if bossstatus == "STATUS: PREPARING" then
+        local args = {[1] = "Boss",[2] = "Ice Golem"}
+        game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
+    wait(15)
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(0) do
+       vim:SendKeyEvent(true, "TELEPORT KEY HERE", false, game)
+       wait(.0)
+peanut = peanut + 1
+if peanut == 1
+then
+    break
+end
+end
+    attack()
+end
+
+-- Attack Function
+function attack()
+local args = {[1] = "UseSpell",[2] = "SPELL HERE",} -- Golden Hearts, Tornado..
+game:GetService("ReplicatedStorage").Server:FireServer(unpack(args))
+wait() -- SPELL'S COOLDOWN!
+end
+
+task.wait(15)
+attack()
+end
